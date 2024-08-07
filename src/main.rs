389ea069441ce56a9c218574eff6e88b8f4e90c1
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2024-08-07 14:02:23 +0200

    Added Extraction of Palette, Sprites and Tiles

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,12 @@ use std::{
 
 use dirs::home_dir;
 
+const DEFAULT_PALETTE: [u8; 48] = [
+    26, 28, 44, 93, 39, 93, 177, 62, 83, 239, 125, 87, 255, 205, 117, 167, 240, 112, 56, 183, 100,
+    37, 113, 121, 41, 54, 111, 59, 93, 201, 65, 166, 246, 115, 239, 247, 244, 244, 244, 148, 176,
+    194, 86, 108, 134, 51, 60, 87,
+];
+
 #[derive(Clone, Debug)]
 struct Chunk {
     bank: u8,
@@ -169,11 +175,24 @@ fn main() {
 
     path_to_conf.push(".local/share/com.nesbox.tic/TIC-80/.local/b09c50c/config.tic");
 
-    // reading the .tic file
-    let chunks: Vec<Chunk> = deconstruct_tic(path_to_conf.to_str().unwrap().into());
+    let mut chunks: Vec<Chunk> = deconstruct_tic(path_to_conf.to_str().unwrap().into());
 
-    let glyphs: Chunk = extract(chunks, "Tiles".into());
-    let sprites: Chunk = extract(chunks, "Sprites".into());
+    if find(chunks.clone(), "Default".into()) {
+        chunks = replace(
+            chunks.clone(),
+            Chunk {
+                bank: 0,
+                data: DEFAULT_PALETTE.to_vec().clone(),
+                name: "Palette".to_string(),
+            },
+        );
+    }
+
+    let glyphs: Chunk = extract(chunks.clone(), "Tiles".into());
+    let sprites: Chunk = extract(chunks.clone(), "Sprites".into());
+    let palette: Chunk = extract(chunks.clone(), "Palette".into());
 
     println!("{:?}", glyphs);
+    println!("{:?}", sprites);
+    println!("{:?}", palette);
 }
